fbbd2b172f03918d5d565278cefe61ee67696f56
Jean Baptiste Favre <jean-baptiste.favre@blablacar.com>
2019-01-24 16:00:48 +0100

    Fix pylint errors & warnings

--- mower.py
+++ mower.py
@@ -1,12 +1,16 @@
 #/usr/bin/env python3
+""" Tech test for BlaBlaCar hiring process
+
+The script implement an undefined number of automatic mowers
+"""
 
 __VERSION__ = '0.1'
 
 import argparse
 import logging
 try:
-    from itertools import zip_longest as zip_longest
-except:
+    from itertools import zip_longest
+except ImportError:
     from itertools import izip_longest as zip_longest
 
 def read_args():
@@ -18,7 +22,8 @@ def read_args():
     description = 'Mower command line switches'
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("-f", "--file", help="File with move instructions", required=True)
-    parser.add_argument("-d", "--debug", help="Enable debug logging", dest='debug', action='store_true')
+    parser.add_argument(
+        "-d", "--debug", help="Enable debug logging", dest='debug', action='store_true')
     return parser.parse_args()
 
 def init_lawn(horizontal, vertical):
@@ -42,9 +47,12 @@ def init_lawn(horizontal, vertical):
     # Create empty map[X][Y]
     # X: horizontal index
     # Y: vertical index
-    lawn = []
-    for i in range(horizontal):
-        lawn.append([None] * vertical)
+    if not isinstance(horizontal, int) or not isinstance(horizontal, int):
+        raise TypeError("Both parmas must be integer")
+    if not horizontal >= 0 or not vertical >= 0:
+        raise ValueError("Both params must be greater or equal to zero")
+
+    lawn = [[None] * vertical for num in range(horizontal)]
 
     return lawn
 
@@ -57,19 +65,22 @@ def init_mower(lawn_map, mower_initial_position, mower_moves_list):
 
     Parameters:
         lawn_map (list): list of lawn columns
-        mower_initial_position (tuple): mower's horizontal & vertical coordinates, mower's orientation
+        mower_initial_position (tuple): mower's horizontal & vertical coordinates,
+            mower's orientation
         mower_moves_list (str): mower's moves to process
     Returns:
         lawn_map (list): list of lawn columns
         mower (dict): mower's position & moves list
     """
 
-    x = int(mower_initial_position[0])
-    y = int(mower_initial_position[1])
+    pos_x = int(mower_initial_position[0])
+    pos_y = int(mower_initial_position[1])
+    pos_o = mower_initial_position[2]
+
     try:
         # Initialize mower and mark its position occupied on the map
         # only if position isn't already filled
-        check_new_position(lawn_map, x, y)
+        check_new_position(pos_x, pos_y, pos_o, lawn_map)
     except ValueError as err:
         # mower next position is already occupied.
         raise err
@@ -80,35 +91,45 @@ def init_mower(lawn_map, mower_initial_position, mower_moves_list):
     # Initialize mower
     mower = {"position": mower_initial_position, "movelist": mower_moves_list}
     # Mark mower's position occupied on the map
-    lawn_map[x][y] = 1
+    lawn_map[pos_x][pos_y] = 1
 
     return lawn_map, mower
 
-def check_new_position(lawn_map, x, y):
+def check_new_position(pos_x, pos_y, pos_o, lawn_map=None):
     """ Check new position validity
 
     This method checks new position validity with 2 rules:
-    - next position should not be outside of the map.  
+    - next position should not be outside of the map.
     It tries to read next position value. If we're outside of the map,
     this will trigger an IndexError exception
     - next position must not be already occupied.
     We use assertion for that, assuming next position value is None.
-    If position is already occupied by another mower, it triggers an AssertionError exception
+    If position is already occupied by another mower, it triggers an
+        AssertionError exception
 
     Parameters:
         lawn_map (list): list of lawn columns
-        x (int): mower's horizontal coordinate
-        y (int): mower's vertical coordinate
+        pos_x (int): mower's horizontal coordinate
+        pos_y (int): mower's vertical coordinate
     """
 
-    try:
-        cur_value = lawn_map[x][y]
-    except:
-        raise IndexError("check_new_position: Can not go out of the map")
-    try:
-        assert(lawn_map[x][y] is None)
-    except:
-        raise ValueError("check_new_position: Position already occupied by another mower")
+    if not isinstance(pos_x, int) or not isinstance(pos_y, int):
+        raise TypeError("Both parmas must be integer")
+    if pos_x < 0 or pos_y < 0:
+        raise ValueError("Both params must be greater or equal to zero")
+    if pos_o not in "NESW":
+        raise ValueError(
+            "Invalid orientation. Got %s, expected one of [N, E, S, W]" % pos_o
+        )
+    if lawn_map is not None:
+        try:
+            cur_value = lawn_map[pos_x][pos_y]
+        except:
+            raise IndexError("Can not go out of the map")
+        try:
+            assert lawn_map[pos_x][pos_y] is None
+        except:
+            raise ValueError("Position already occupied by another mower")
 
 def get_next_orientation(orientation, move):
     """ Compute next mower orientation
@@ -131,7 +152,7 @@ def get_next_orientation(orientation, move):
     if move == "L":
         offset = int(orientation_index - 1)
     if move == "R":
-        offset =  int(orientation_index + 1)
+        offset = int(orientation_index + 1)
 
     return orientation_list[(len(orientation_list) + offset) % len(orientation_list)]
 
@@ -186,7 +207,7 @@ def move_mower(lawn_map, mower):
     """
 
     for move in mower["movelist"]:
-        x, y, orientation = mower["position"]
+        cur_x, cur_y, orientation = mower["position"]
         new_x, new_y, new_orientation = mower["position"]
         if move in ["L", "R"]:
             # Updating orientation without moving
@@ -197,7 +218,7 @@ def move_mower(lawn_map, mower):
             # Get new position
             # Possible error when passing bad orientation as argument
             try:
-                new_x, new_y = get_next_position(int(x), int(y), orientation)
+                new_x, new_y = get_next_position(int(cur_x), int(cur_y), orientation)
             except ValueError as err:
                 # mower next position is already occupied.
                 # Cancelling the move, but continue processing
@@ -207,8 +228,8 @@ def move_mower(lawn_map, mower):
                 # Check that next position is valid
                 # Possible error if position outside of the map
                 # or position already occupied by another mower
-                check_new_position(lawn_map, int(new_x), int(new_y))
-            except Exception as err:
+                check_new_position(int(new_x), int(new_y), new_orientation, lawn_map)
+            except (TypeError, IndexError, ValueError) as err:
                 # Either position is outside the map,
                 # or new position is already occupied by another mower
                 # Cancel the move, but continue processing
@@ -216,18 +237,18 @@ def move_mower(lawn_map, mower):
                 continue
             # Move succeeded. Freeing old map location
             lawn_map[int(new_x)][int(new_y)] = 1
-            lawn_map[int(x)][int(y)] = None
+            lawn_map[int(cur_x)][int(cur_y)] = None
         else:
             raise ValueError("Invalid move command. Got %s, expected one of [L, R, F]" % move)
         # Check new position
         # Possibly get IndexError when trying to go outside of the map
         #              ValueError if next location is already occupied by another mower
         logging.debug(
-            "Current position: %s. Next position: %s after move %s)" %
-            (mower["position"], (str(new_x), str(new_y), new_orientation), move)
+            "Current position: %s. Next position: %s after move %s)",
+            mower["position"], (str(new_x), str(new_y), new_orientation), move
         )
         mower["position"] = (new_x, new_y, new_orientation)
-    logging.debug("Final   position: %s" % str(mower["position"]))
+    logging.debug("Final   position: %s", str(mower["position"]))
 
     return lawn_map, mower
 
@@ -282,7 +303,7 @@ def main():
             except IndexError as err:
                 print("Mower %d: %s" % (mowers, err))
                 continue
-            logging.debug("Mower %d created" % mowers)
+            logging.debug("Mower %d created", mowers)
             try:
                 lawn_map, mower = move_mower(lawn_map, mower)
             except ValueError as err:
