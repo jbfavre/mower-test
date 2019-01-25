b0a890973c0117fab515d34b3c99038d92649e99
Jean Baptiste Favre <jean-baptiste.favre@blablacar.com>
2019-01-25 15:57:57 +0100

    Add move_mower tests

--- test/test_move_mower.py
+++ test/test_move_mower.py
@@ -6,17 +6,25 @@
 #
 #    return lawn_map, mower
 import pytest
-
+import mock
 from mower import move_mower
 
 def generate_empty_map():
     return [[None] * 15 for num in range(12)]
 
-def test_move_mower():
+@mock.patch("mower.get_next_orientation")
+@mock.patch("mower.get_next_position")
+@mock.patch("mower.check_new_position")
+def test_move_mower(mock_check_new_position, mock_get_next_position, mock_get_next_orientation):
     fake_map = generate_empty_map()
     fake_map[0][0] == 1
     fake_mower = {"position": (0, 0, "E"), "movelist": "FLFFR"}
-    lawn_map, mower = move_mower(fake_map, fake_mower)
-    assert lawn_map[0][0] is None
-    assert lawn_map[1][2] == 1
-    assert mower == {"position": ('1', '2', "E"), "movelist": "FLFFR"}
+
+    mock_get_next_orientation.side_effect = ["N", "E"]
+    mock_get_next_position.side_effect = [(1, 0), (1, 1), (1, 2)]
+    mock_check_new_position.side_effect = [None, None, None]
+    with mock_get_next_orientation, mock_get_next_position, mock_check_new_position:
+        test_lawn_map, test_mower = move_mower(fake_map, fake_mower)
+    assert test_lawn_map[0][0] is None
+    assert test_lawn_map[1][2] == 1
+    assert test_mower == {"position": (1, 2, "E"), "movelist": "FLFFR"}
